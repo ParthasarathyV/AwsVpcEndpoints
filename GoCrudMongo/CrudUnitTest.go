d6cf3b5abf49ab50a44c3bf5f25ae5c782be1c8d
ParthasarathyV <114770988+ParthasarathyV@users.noreply.github.com>
2023-08-07 19:17:13 +0530

    Update CrudUnitTest.go

--- GoCrudMongo/CrudUnitTest.go
+++ GoCrudMongo/CrudUnitTest.go
@@ -1,4 +1,4 @@
-package crud
+package main
 
 import (
 	"context"
@@ -6,93 +6,50 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/mock"
 )
 
-type MockCollection struct {
-	mock.Mock
-}
-
-func (m *MockCollection) InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
-	args := m.Called(ctx, document, opts)
-	return args.Get(0).(*mongo.InsertOneResult), args.Error(1)
-}
-
-func (m *MockCollection) FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) SingleResult {
-	args := m.Called(ctx, filter, opts)
-	return args.Get(0).(SingleResult)
-}
-
-func (m *MockCollection) UpdateOne(ctx context.Context, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
-	args := m.Called(ctx, filter, update, opts)
-	return args.Get(0).(*mongo.UpdateResult), args.Error(1)
-}
-
-func (m *MockCollection) DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
-	args := m.Called(ctx, filter, opts)
-	return args.Get(0).(*mongo.DeleteResult), args.Error(1)
-}
-
-type MockSingleResult struct {
-	mock.Mock
-}
-
-func (m *MockSingleResult) Decode(v interface{}) error {
-	args := m.Called(v)
-	return args.Error(0)
-}
-
 func TestCRUDOperations(t *testing.T) {
-	mockSingleResult := new(MockSingleResult)
-	mockCollection := new(MockCollection)
+	mockClient := mock.NewClient()
+	db := mockClient.Database("testdb")
+	repo := NewUserRepository(db)
 
-	user := User{
-		ID:        "123",
-		Name:      "John",
+	// Create
+	user := &User{
+		Username:  "john_doe",
 		Email:     "john@example.com",
 		CreatedAt: time.Now(),
 	}
-
-	// Set up Create mock
-	mockCollection.On("InsertOne", mock.Anything, mock.Anything, mock.Anything).Return(&mongo.InsertOneResult{}, nil)
-
-	// Set up Read mock
-	mockSingleResult.On("Decode", mock.AnythingOfType("*crud.User")).Run(func(args mock.Arguments) {
-		ptr := args.Get(0).(*User)
-		*ptr = user
-	}).Return(nil)
-	mockCollection.On("FindOne", mock.Anything, mock.Anything, mock.Anything).Return(mockSingleResult)
-
-	// Set up Update mock
-	mockCollection.On("UpdateOne", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&mongo.UpdateResult{}, nil)
-
-	// Set up Delete mock
-	mockCollection.On("DeleteOne", mock.Anything, mock.Anything, mock.Anything).Return(&mongo.DeleteResult{}, nil)
-
-	createClient = func() (*mongo.Client, error) {
-		return &mongo.Client{}, nil
-	}
-
-	service := &MongoDBService{collection: mockCollection}
-
-	// Test Create
-	err := service.Create(user)
+	err := repo.Create(user)
 	assert.NoError(t, err)
+	assert.NotEmpty(t, user.ID)
 
-	// Test Read
-	readUser, err := service.Read(user.ID)
+	// Read
+	readUser, err := repo.Read(user.ID)
 	assert.NoError(t, err)
 	assert.NotNil(t, readUser)
-	assert.Equal(t, user.Name, readUser.Name)
+	assert.Equal(t, user.Username, readUser.Username)
 
-	// Test Update
-	err = service.Update(user.ID, "Updated John", "updated@example.com")
+	// Update
+	updatedUsername := "jane_doe"
+	readUser.Username = updatedUsername
+	err = repo.Update(readUser)
 	assert.NoError(t, err)
 
-	// Test Delete
-	err = service.Delete(user.ID)
+	// Read updated user
+	updatedReadUser, err := repo.Read(user.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, updatedReadUser)
+	assert.Equal(t, updatedUsername, updatedReadUser.Username)
+
+	// Delete
+	err = repo.Delete(user.ID)
 	assert.NoError(t, err)
 
-	mockCollection.AssertExpectations(t)
-	mockSingleResult.AssertExpectations(t)
+	// Verify deletion
+	deletedUser, err := repo.Read(user.ID)
+	assert.Error(t, err)
+	assert.Nil(t, deletedUser)
 }
