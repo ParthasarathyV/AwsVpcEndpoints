85ae0d0d11f5d464dc5fcdfa17e63b3d2f525575
ParthasarathyV <114770988+ParthasarathyV@users.noreply.github.com>
2023-08-07 20:03:50 +0530

    Create crud_test.go

---
+++ GoCrudMongo/NewCrud/crud_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestIntegrationCRUDOperations(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().Topologies(mtest.ReplicaSet))
+	defer mt.Close()
+
+	// Get the client from the mtest context
+	client := mt.Client()
+
+	// Create UserRepository using the client
+	repo := NewUserRepository(client, "testdb", "users")
+
+	ctx := context.Background()
+
+	// Create
+	user := &User{
+		Username:  "john_doe",
+		Email:     "john@example.com",
+		CreatedAt: time.Now(),
+	}
+	err := repo.Create(ctx, user)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, user.ID)
+
+	// Read
+	readUser, err := repo.Read(ctx, user.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, readUser)
+	assert.Equal(t, user.Username, readUser.Username)
+
+	// Update
+	updatedUsername := "jane_doe"
+	readUser.Username = updatedUsername
+	err = repo.Update(ctx, readUser)
+	assert.NoError(t, err)
+
+	// Read updated user
+	updatedReadUser, err := repo.Read(ctx, user.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, updatedReadUser)
+	assert.Equal(t, updatedUsername, updatedReadUser.Username)
+
+	// Delete
+	err = repo.Delete(ctx, user.ID)
+	assert.NoError(t, err)
+
+	// Verify deletion
+	deletedUser, err := repo.Read(ctx, user.ID)
+	assert.Error(t, err)
+	assert.Nil(t, deletedUser)
+}
