e86035b99847fdd023c49f47533d9a059c434f31
ParthasarathyV <114770988+ParthasarathyV@users.noreply.github.com>
2023-08-07 18:50:38 +0530

    Update CrudUnitTest.go

--- GoCrudMongo/CrudUnitTest.go
+++ GoCrudMongo/CrudUnitTest.go
@@ -7,60 +7,92 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type MockCollection struct {
+	mock.Mock
+}
+
+func (m *MockCollection) InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
+	args := m.Called(ctx, document, opts)
+	return args.Get(0).(*mongo.InsertOneResult), args.Error(1)
+}
+
+func (m *MockCollection) FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) SingleResult {
+	args := m.Called(ctx, filter, opts)
+	return args.Get(0).(SingleResult)
+}
+
+func (m *MockCollection) UpdateOne(ctx context.Context, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
+	args := m.Called(ctx, filter, update, opts)
+	return args.Get(0).(*mongo.UpdateResult), args.Error(1)
+}
+
+func (m *MockCollection) DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
+	args := m.Called(ctx, filter, opts)
+	return args.Get(0).(*mongo.DeleteResult), args.Error(1)
+}
+
+type MockSingleResult struct {
+	mock.Mock
+}
+
+func (m *MockSingleResult) Decode(v interface{}) error {
+	args := m.Called(v)
+	return args.Error(0)
+}
+
 func TestCRUDOperations(t *testing.T) {
-	// Create mock objects
 	mockSingleResult := new(MockSingleResult)
 	mockCollection := new(MockCollection)
-	mockDatabase := new(MockDatabase)
-	mockClient := new(MockClient)
 
-	// Set up mock interactions
-	mockSingleResult.On("Decode", mock.AnythingOfType("*crud.User")).Return(nil)
-	mockCollection.On("InsertOne", mock.Anything, mock.Anything, mock.Anything).Return(&mongo.InsertOneResult{}, nil)
-	mockCollection.On("FindOne", mock.Anything, mock.Anything, mock.Anything).Return(mockSingleResult)
-	mockCollection.On("UpdateOne", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&mongo.UpdateResult{}, nil)
-	mockCollection.On("DeleteOne", mock.Anything, mock.Anything, mock.Anything).Return(&mongo.DeleteResult{}, nil)
-	mockDatabase.On("Collection", mock.Anything).Return(mockCollection)
-	mockClient.On("Disconnect", mock.Anything).Return(nil)
-	mockClient.On("Database", mock.Anything).Return(mockDatabase)
-
-	// Set up createClient to return the mock client
-	clientFn := func(context.Context, ...*options.ClientOptions) (*mongo.Client, error) {
-		return mockClient, nil
-	}
-	createClient = clientFn
-
-	// Test Create
 	user := User{
 		ID:        "123",
 		Name:      "John",
 		Email:     "john@example.com",
 		CreatedAt: time.Now(),
 	}
-	err := CreateUser(user)
+
+	// Set up Create mock
+	mockCollection.On("InsertOne", mock.Anything, mock.Anything, mock.Anything).Return(&mongo.InsertOneResult{}, nil)
+
+	// Set up Read mock
+	mockSingleResult.On("Decode", mock.AnythingOfType("*crud.User")).Run(func(args mock.Arguments) {
+		ptr := args.Get(0).(*User)
+		*ptr = user
+	}).Return(nil)
+	mockCollection.On("FindOne", mock.Anything, mock.Anything, mock.Anything).Return(mockSingleResult)
+
+	// Set up Update mock
+	mockCollection.On("UpdateOne", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&mongo.UpdateResult{}, nil)
+
+	// Set up Delete mock
+	mockCollection.On("DeleteOne", mock.Anything, mock.Anything, mock.Anything).Return(&mongo.DeleteResult{}, nil)
+
+	createClient = func() (*mongo.Client, error) {
+		return &mongo.Client{}, nil
+	}
+
+	service := &MongoDBService{collection: mockCollection}
+
+	// Test Create
+	err := service.Create(user)
 	assert.NoError(t, err)
 
 	// Test Read
-	readUser, err := ReadUser("123")
+	readUser, err := service.Read(user.ID)
 	assert.NoError(t, err)
 	assert.NotNil(t, readUser)
 	assert.Equal(t, user.Name, readUser.Name)
 
 	// Test Update
-	err = UpdateUser("123", "Updated John", "updated@example.com")
+	err = service.Update(user.ID, "Updated John", "updated@example.com")
 	assert.NoError(t, err)
 
 	// Test Delete
-	err = DeleteUser("123")
+	err = service.Delete(user.ID)
 	assert.NoError(t, err)
 
-	// Verify mock interactions
-	mockClient.AssertExpectations(t)
-	mockDatabase.AssertExpectations(t)
 	mockCollection.AssertExpectations(t)
 	mockSingleResult.AssertExpectations(t)
 }
