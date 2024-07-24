b82cbccfd602f8f87898c9636b6cdc48cd5c85c8
Parthasarathy Varadhan <parthasarathy.varadhan@peerislands.io>
2024-07-24 16:37:56 -0500

    Correct

--- terraformSnippets/vpcendpoints/locals.tf
+++ terraformSnippets/vpcendpoints/locals.tf
@@ -6,41 +6,41 @@ locals {
 
 locals {
     im_recordings_s3 = {
-  bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["im-recordings-s3"], "bucket_name","")
-  bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["im-recordings-s3"], "bucket_arn","")
+  bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.im-recordings-s3, "bucket_name","")
+  bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.im-recordings-s3, "bucket_arn","")
 }
   imediapci_vc_recordings_3y_s3 = {
-    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-3y-s3"], "bucket_name", "")
-    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-3y-s3"], "bucket_arn", "")
+    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_3y_s3, "bucket_name", "")
+    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_3y_s3, "bucket_arn", "")
   }
   
   imediapci_vc_recordings_13m_s3 = {
-    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-13m-s3"], "bucket_name", "")
-    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-13m-s3"], "bucket_arn", "")
+    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_13m_s3, "bucket_name", "")
+    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_13m_s3, "bucket_arn", "")
   }
   
   imediapci_vc_recordings_60d_s3 = {
-    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-60d-s3"], "bucket_name", "")
-    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-60d-s3"], "bucket_arn", "")
+    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_60d_s3, "bucket_name", "")
+    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_60d_s3, "bucket_arn", "")
   }
   
   imediapci_vc_recordings_2y_s3 = {
-    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-2y-s3"], "bucket_name", "")
-    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-recordings-2y-s3"], "bucket_arn", "")
+    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_2y_s3, "bucket_name", "")
+    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_recordings_2y_s3, "bucket_arn", "")
   }
   
   imediapci_lh_s3 = {
-    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-lh-s3"], "bucket_name", "")
-    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-lh-s3"], "bucket_arn", "")
+    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_lh_s3, "bucket_name", "")
+    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_lh_s3, "bucket_arn", "")
   }
   
   imediapci_vc_transcripts_13m_s3 = {
-    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-transcripts-13m-s3"], "bucket_name", "")
-    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-transcripts-13m-s3"], "bucket_arn", "")
+    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_transcripts_13m_s3, "bucket_name", "")
+    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_transcripts_13m_s3, "bucket_arn", "")
   }
   
   imediapci_vc_transcripts_3y_s3 = {
-    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-transcripts-3y-s3"], "bucket_name", "")
-    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs["imediapci-vc-transcripts-3y-s3"], "bucket_arn", "")
+    bucket_name = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_transcripts_3y_s3, "bucket_name", "")
+    bucket_arn  = lookup(data.terraform_remote_state[local.workspace_name].outputs.imediapci_vc_transcripts_3y_s3, "bucket_arn", "")
   }
 }
