563d6d5d545f0014bbd9998aaee1d41adf488330
Parthasarathy <psvpartha@gmail.com>
2024-12-11 21:23:17 +0530

    Update temp.js

--- Tasks/pivotInMongo/temp.js
+++ Tasks/pivotInMongo/temp.js
@@ -20,3 +20,43 @@
       }
     }
   }
+
+
+
+[
+  {
+    "$addFields": {
+      "dynamicFields1": {
+        "$arrayToObject": {
+          "$filter": {
+            "input": {
+              "$map": {
+                "input": "$dp1",
+                "as": "item",
+                "in": {
+                  "k": "$$item.k",
+                  "v": {
+                    "$reduce": {
+                      "input": {
+                        "$filter": {
+                          "input": "$dp1",
+                          "as": "filteredItem",
+                          "cond": { "$ne": ["$$filteredItem.k", null] }
+                        }
+                      },
+                      "initialValue": 0,
+                      "in": { "$add": ["$$value", "$$this.v"] }
+                    }
+                  }
+                }
+              }
+            },
+            "as": "mappedItem",
+            "cond": { "$ne": ["$$mappedItem.k", null] }
+          }
+        }
+      }
+    }
+  }
+]
+
