ee2dd64502e14e7f87e6b22725ca179fd75c0091
Parthasarathy <psvpartha@gmail.com>
2024-12-17 19:22:14 +0530

    Update StaticAgg.java

--- Tasks/pivotInMongo/StaticAgg.java
+++ Tasks/pivotInMongo/StaticAgg.java
@@ -10,55 +10,28 @@ public class MongoAggregationExample {
         List<AggregationOperation> aggOps = new ArrayList<>();
 
         // $group stage
-        GroupOperation groupStage = group(
-                Fields.fields("type", "benefitsReportingLevel", "state", "rag"))
+        GroupOperation groupStage = group(Fields.fields("type", "benefitsReportingLevel", "state", "rag"))
                 .sum("forecastTotalCostNextYear").as("forecastTotalCostNextYear")
                 .sum("totalCostNextYear").as("totalCostNextYear");
         aggOps.add(groupStage);
 
         // $addFields stage for pivotField1 and pivotField2
-        AddFieldsOperation addFieldsStage1 = AddFieldsOperation.builder()
-                .addField("pivotField1",
-                        ValueOperators.Concat.valueOf("_id.state")
-                                .concat("-")
-                                .concat(ValueOperators.Concat.valueOf("_id.rag"))
-                                .concat("-forecastTotalCostNextYear"))
-                .addField("pivotField2",
-                        ValueOperators.Concat.valueOf("_id.state")
-                                .concat("-")
-                                .concat(ValueOperators.Concat.valueOf("_id.rag"))
-                                .concat("-totalCostNextYear"))
-                .build();
+        ProjectionOperation addFieldsStage1 = project()
+                .andExpression("concat($_id.state, '-', $_id.rag, '-forecastTotalCostNextYear)").as("pivotField1")
+                .andExpression("concat($_id.state, '-', $_id.rag, '-totalCostNextYear)").as("pivotField2")
+                .andExclude("_id");
         aggOps.add(addFieldsStage1);
 
         // $addFields stage for rowData
-        AddFieldsOperation addFieldsStage2 = AddFieldsOperation.builder()
-                .addField("rowData", AccumulatorOperators.MergeObjects.merge(
-                        List.of(
-                                ValueOperators.ObjectLiteral.newObjectLiteral()
-                                        .field("type", "_id.type").build(),
-                                ValueOperators.ObjectLiteral.newObjectLiteral()
-                                        .field("benefitsReportingLevel", "_id.benefitsReportingLevel").build(),
-                                AccumulatorOperators.ArrayToObject.arrayToObject(
-                                        ArrayOperators.ArrayOf.arrayOf(
-                                                List.of(
-                                                        ValueOperators.ObjectLiteral.newObjectLiteral()
-                                                                .field("k", "pivotField1")
-                                                                .field("v", "forecastTotalCostNextYear").build()
-                                                )
-                                        )
-                                ),
-                                AccumulatorOperators.ArrayToObject.arrayToObject(
-                                        ArrayOperators.ArrayOf.arrayOf(
-                                                List.of(
-                                                        ValueOperators.ObjectLiteral.newObjectLiteral()
-                                                                .field("k", "pivotField2")
-                                                                .field("v", "totalCostNextYear").build()
-                                                )
-                                        )
-                                )
-                        )
-                )).build();
+        ProjectionOperation addFieldsStage2 = project()
+                .and("$_id.type").as("rowData.type")
+                .and("$_id.benefitsReportingLevel").as("rowData.benefitsReportingLevel")
+                .andExpression(
+                        "{ $arrayToObject: [[{ k: pivotField1, v: forecastTotalCostNextYear }]] }"
+                ).as("rowData.forecastData")
+                .andExpression(
+                        "{ $arrayToObject: [[{ k: pivotField2, v: totalCostNextYear }]] }"
+                ).as("rowData.totalCostData");
         aggOps.add(addFieldsStage2);
 
         // $group stage for aggregating pivotFields and rows
@@ -70,9 +43,7 @@ public class MongoAggregationExample {
 
         // $project stage to construct the final response
         ProjectionOperation projectStage = project()
-                .and(
-                        ArrayOperators.ConcatArrays.concatArrays("pivotFields1", "pivotFields2")
-                ).as("response.pivotFields")
+                .and(ArrayOperators.ArrayConcat.concat("pivotFields1", "pivotFields2")).as("response.pivotFields")
                 .and("rows").as("response.rows");
         aggOps.add(projectStage);
 
