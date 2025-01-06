d05826cc88f0234e13d458772b8c82622efbd752
Parthasarathy <psvpartha@gmail.com>
2025-01-07 01:18:11 +0530

    Create pojo.java

---
+++ Tasks/pivotInMongo/openAPIOverride/pojo.java
@@ -0,0 +1,12 @@
+public class CustomRequest {
+    private String data;
+
+    // Getters and Setters
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
