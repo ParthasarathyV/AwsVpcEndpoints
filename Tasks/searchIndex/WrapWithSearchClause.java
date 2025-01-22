23ee83e035d7f1469358d7791fcb50a4f86b8e9d
Parthasarathy <psvpartha@gmail.com>
2025-01-22 17:29:09 +0530

    Update WrapWithSearchClause.java

--- Tasks/searchIndex/WrapWithSearchClause.java
+++ Tasks/searchIndex/WrapWithSearchClause.java
@@ -20,17 +20,25 @@ public List<AggregationOperation> toAggregationOperations(String fieldName, Text
 }
 
 private List<AggregationOperation> wrapWithSearchClause(List<AggregationOperation> aggOps) {
-    // Create the $search operation
-    AggregationOperation searchOperation = context -> new Document("$search",
-        new Document("index", "filter_index") // Specify your Atlas Search index name here
-            .append("compound", new Document("must", aggOps.stream()
-                .map(AggregationOperation::toDocument) // Convert aggOps to a list of Documents
-                .toList())));
+    // Extract the inner $search query from aggOps
+    List<Document> compoundConditions = new ArrayList<>();
+    for (AggregationOperation aggOp : aggOps) {
+        // Apply each operation's context to create a Document and extract the $search condition
+        Document operation = aggOp.toDocument(AggregationOperationContext.DEFAULT);
+        if (operation.containsKey("$search")) {
+            compoundConditions.add((Document) operation.get("$search"));
+        }
+    }
 
-    // Add the $search operation at the beginning of the list
+    // Create a new $search operation wrapping all conditions
+    Document searchWithIndex = new Document("$search",
+        new Document("index", "filter_index") // Specify your Atlas Search index
+            .append("compound", new Document("must", compoundConditions)));
+
+    AggregationOperation searchOperation = context -> searchWithIndex;
+
+    // Return a new list with the $search operation at the beginning
     List<AggregationOperation> wrappedOps = new ArrayList<>();
     wrappedOps.add(searchOperation);
-    wrappedOps.addAll(aggOps);
-
     return wrappedOps;
 }
