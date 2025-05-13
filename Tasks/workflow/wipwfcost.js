941f84f3a62cfc500cc922772976fa0a70aeabcd
Parthasarathy <psvpartha@gmail.com>
2025-05-13 16:59:42 -0500

    Update wipwfcost.js

--- Tasks/workflow/wipwfcost.js
+++ Tasks/workflow/wipwfcost.js
@@ -1,81 +1,58 @@
 db.collection.aggregate([
-  { $unwind: "$costs" },
+  // Step 1: Flatten costs
+  { $unwind: { path: "$costs", preserveNullAndEmptyArrays: true } },
+
+  // Step 2: Group by doc _id + source + type
   {
     $group: {
       _id: {
-        docId: "$_id",
+        _id: "$_id",
         source: "$costs.source",
         type: "$costs.type"
       },
-      typeTotal: { $sum: { $ifNull: ["$costs.fycost", 0] } }
+      fycost: { $sum: { $ifNull: ["$costs.fycost", 0] } },
+      root: { $first: "$$ROOT" }
     }
   },
+
+  // Step 3: Group by doc _id + source, accumulate details array
   {
     $group: {
       _id: {
-        docId: "$_id.docId",
+        _id: "$_id._id",
         source: "$_id.source"
       },
-      total: { $sum: "$typeTotal" },
+      total: { $sum: "$fycost" },
       details: {
         $push: {
           type: "$_id.type",
-          total: "$typeTotal"
+          total: "$fycost"
         }
-      }
+      },
+      root: { $first: "$root" }
     }
   },
+
+  // Step 4: Group by document _id to assemble new costs array
   {
     $group: {
-      _id: "$_id.docId",
-      costs: {
+      _id: "$_id._id",
+      newCosts: {
         $push: {
           source: "$_id.source",
           total: "$total",
           details: "$details"
         }
-      }
+      },
+      root: { $first: "$root" }
     }
   },
+
+  // Step 5: Reconstruct full document with replaced costs
   {
     $replaceRoot: {
       newRoot: {
-        $mergeObjects: [
-          { _id: "$_id" },
-          {
-            $arrayElemAt: [
-              {
-                $map: {
-                  input: [
-                    {
-                      $mergeObjects: [
-                        "$$ROOT",
-                        {
-                          $arrayToObject: {
-                            $filter: {
-                              input: {
-                                $map: {
-                                  input: { $objectToArray: "$$ROOT" },
-                                  as: "pair",
-                                  in: "$$pair"
-                                }
-                              },
-                              as: "item",
-                              cond: { $ne: ["$$item.k", "costs"] }
-                            }
-                          }
-                        }
-                      ]
-                    }
-                  ],
-                  as: "doc",
-                  in: "$$doc"
-                }
-              },
-              0
-            ]
-          }
-        ]
+        $mergeObjects: ["$root", { costs: "$newCosts" }]
       }
     }
   }
