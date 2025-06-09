a313c5d6f633704441b699c8a0e0d166ce20ab90
Parthasarathy <psvpartha@gmail.com>
2025-06-09 11:04:06 -0500

    Update aggBuilderFromFile.java

--- Tasks/financials/l2/aggBuilderFromFile.java
+++ Tasks/financials/l2/aggBuilderFromFile.java
@@ -1,16 +1,23 @@
 public Aggregation fetchL1ToL2aggregation(String proposalId) throws IOException {
     String path = "mongqueries/L1ToL2.js";
-    InputStream is = getClass().getClassLoader().getResourceAsStream(path);
-    String json = new BufferedReader(new InputStreamReader(is))
-                      .lines().collect(Collectors.joining("\n"));
 
-    List<AggregationOperation> stages = Stream.concat(
-        Stream.of(Aggregation.match(Criteria.where("ipLongId").is(proposalId))),
-        BsonArray.parse(json).stream()
-            .map(BsonValue::asDocument)
-            .map(Document::parse)
-            .map(doc -> (AggregationOperation) context -> doc)
-    ).collect(Collectors.toList());
+    try (InputStream is = getClass().getClassLoader().getResourceAsStream(path)) {
+        if (is == null) {
+            throw new FileNotFoundException("Resource not found: " + path);
+        }
 
-    return Aggregation.newAggregation(stages);
+        String json = new BufferedReader(
+                new InputStreamReader(is, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        List<AggregationOperation> stages = Stream.concat(
+                Stream.of(Aggregation.match(Criteria.where("ipLongId").is(proposalId))),
+                BsonArray.parse(json).stream()
+                        .map(bv -> Document.parse(bv.asDocument().toJson()))
+                        .map(doc -> (AggregationOperation) ctx -> doc)
+        ).collect(Collectors.toList());
+
+        return Aggregation.newAggregation(stages);
+    }
 }
