fc59833918af4388d708b8be87b76898b22b81af
Parthasarathy <psvpartha@gmail.com>
2025-09-30 19:20:45 +0530

    Update formatL3V2.js

--- Tasks/financials/SummaryAPI/formatL3V2.js
+++ Tasks/financials/SummaryAPI/formatL3V2.js
@@ -1,42 +1,70 @@
 [
-  { 
-    $unwind: "$costs" 
-  },
   {
-    $group: {
-      _id: { proposalId: "$proposalId", src: "$costs.src", type: "$costs.type" },
-      total: { $sum: "$costs.fyCost" } // or fyTot / ytdTot depending on field you want
-    }
-  },
-  {
-    $group: {
-      _id: { proposalId: "$_id.proposalId", src: "$_id.src" },
-      details: {
-        $push: {
-          type: "$_id.type",
-          total: "$total"
-        }
-      },
-      total: { $sum: "$total" }
-    }
-  },
-  {
-    $group: {
-      _id: "$_id.proposalId",
-      costs: {
-        $push: {
-          source: "$_id.src",
-          total: "$total",
-          details: "$details"
-        }
-      }
+    $match: {
+      proposalId: "8710b8b1-4d07-48ba-98b6-ce731888d37e",
+      planId: "8263cc2d-e38d-46e5-a305-620460857f29",
+      year: 2024
     }
   },
   {
     $project: {
-      _id: 0,
-      proposalId: "$_id",
-      costs: 1
+      proposalId: 1,
+      planId: 1,
+      scenario: 1,
+      years: 1,
+      costs: {
+        $map: {
+          input: { $setUnion: ["$costs.src", []] }, // unique sources
+          as: "srcVal",
+          in: {
+            source: "$$srcVal",
+            details: {
+              $map: {
+                input: { $setUnion: ["$costs.type", []] }, // unique types
+                as: "typeVal",
+                in: {
+                  type: "$$typeVal",
+                  total: {
+                    $sum: {
+                      $map: {
+                        input: {
+                          $filter: {
+                            input: "$costs",
+                            as: "c",
+                            cond: {
+                              $and: [
+                                { $eq: ["$$c.src", "$$srcVal"] },
+                                { $eq: ["$$c.type", "$$typeVal"] }
+                              ]
+                            }
+                          }
+                        },
+                        as: "c2",
+                        in: "$$c2.fyCost"
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            total: {
+              $sum: {
+                $map: {
+                  input: {
+                    $filter: {
+                      input: "$costs",
+                      as: "c3",
+                      cond: { $eq: ["$$c3.src", "$$srcVal"] }
+                    }
+                  },
+                  as: "c4",
+                  in: "$$c4.fyCost"
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 ]
